7cae2eed77a0f7fc398d2b76c3070b2abf273790
Kristopher Urie <kurie@fieldmuseum.org>
2011-04-22 10:14:55 -0500

    Refactoring

--- js/layout/treemap.js
+++ js/layout/treemap.js
@@ -1,48 +1,49 @@
 var squarifiedTreemap = {
 
-	doLayout: function doLayout(childContainer, areaCalc, setBounds) {
-		var layoutBounds = {
-				x: 0,
-				y: 0,
-				width: childContainer.width(),
-				height: childContainer.height()
-			},
-			children = jQuery.makeArray(childContainer.children("div.node")), //TODO pass children and bounds in separately so we don't have to assume childContainer is a jQuery object?
+	doLayout: function doLayout(parent, nodeOps) {
+		var layoutBounds = nodeOps.getLayoutBounds(parent),
+			children = nodeOps.getChildren(parent),
 			layoutArea = layoutBounds.width * layoutBounds.height,
-			totalChildArea = children.reduce(function (accumulate, value) {return accumulate + areaCalc(value); }, 0),
+			totalChildArea = children.reduce(function (accumulate, value) {return accumulate + nodeOps.getDisplayArea(value); }, 0),
 			scale = layoutArea / totalChildArea,
-			scaledAreaCalc = function (node) {
-				return scale * areaCalc(node);
-			};
+			scaledNodeOps = this.shallowCopy(nodeOps),
+			child;
+			
+		/* 
+		 * Replace nodeOps with one that re-scales the child areas to fill this parent's layout area 
+		 * (This will be used in squarify, but not passed on to the recursive call to doLayout)
+		 */
+		scaledNodeOps.getDisplayArea = function scaledAreaCalc(node) {
+			return scale * nodeOps.getDisplayArea(node);
+		};
 
 		if (children && layoutArea > 0) {
-			children.sort(function (a, b) { return areaCalc(b) - areaCalc(a); }); //sort by area, descending
-			this.squarify(children, [], layoutBounds, scaledAreaCalc, setBounds);
+			children.sort(function (a, b) { return nodeOps.getDisplayArea(b) - nodeOps.getDisplayArea(a); }); //sort by area, descending
+			this.squarify(children, [], layoutBounds, scaledNodeOps);
 
-			jQuery.each(children, function () {
-				var container = jQuery(this).children("div.body");
-				squarifiedTreemap.doLayout(container, areaCalc, setBounds);
-			});
+			for (child = 0; child < children.length; child++) {
+				this.doLayout(children[child], nodeOps);
+			}
 		}
 	},	
 
-	squarify: function squarify(children, row, bounds, areaCalc, setBounds) {
+	squarify: function squarify(children, row, bounds, nodeOps) {
 		var c = children[0],
 			w = Math.min(bounds.width, bounds.height),
 			newBounds;
 
 		if (children.length === 0) {
 			if (row.length !== 0) {
-				this.layoutRow(row, bounds, areaCalc, setBounds);
+				this.layoutRow(row, bounds, nodeOps);
 			}
 			return;	
 		}
 
-		if (this.worst(row, w, areaCalc) >= this.worst(row.concat([c]), w, areaCalc)) {
-			this.squarify(children.slice(1), row.concat([c]), bounds, areaCalc, setBounds);
+		if (this.worst(row, w, nodeOps.getDisplayArea) >= this.worst(row.concat([c]), w, nodeOps.getDisplayArea)) {
+			this.squarify(children.slice(1), row.concat([c]), bounds, nodeOps);
 		} else {
-			newBounds = this.layoutRow(row, bounds, areaCalc, setBounds);
-			this.squarify(children, [], newBounds, areaCalc, setBounds);
+			newBounds = this.layoutRow(row, bounds, nodeOps);
+			this.squarify(children, [], newBounds, nodeOps);
 		}
 	},
 
@@ -62,14 +63,14 @@ var squarifiedTreemap = {
 		return Math.max(w * w * r.max / (r.sum * r.sum), r.sum * r.sum / (w * w * r.min));
 	},
 	
-	layoutRow: function layoutRow(row, bounds, areaCalc, setBounds) {
+	layoutRow: function layoutRow(row, bounds, nodeOps) {
 		var rowBounds = {
 				x: bounds.x,
 				y: bounds.y,
 				width: 0,
 				height: 0
 			},
-			rowSum = row.reduce(function (accumulate, value) {return accumulate + areaCalc(value); }, 0),
+			rowSum = row.reduce(function (accumulate, value) {return accumulate + nodeOps.getDisplayArea(value); }, 0),
 			dir;
 
 		if (bounds.width > bounds.height) {
@@ -86,12 +87,12 @@ var squarifiedTreemap = {
 			dir = "h";
 		}
 
-		this.layoutNodes(row, rowBounds, dir, areaCalc, setBounds);
+		this.layoutNodes(row, rowBounds, dir, nodeOps);
 
 		return bounds;
 	},
 
-	layoutNodes: function layoutNodes(row, rowBounds, dir, areaCalc, setBounds) {
+	layoutNodes: function layoutNodes(row, rowBounds, dir, nodeOps) {
 		if (row.length === 0) {
 			return;
 		}
@@ -105,27 +106,41 @@ var squarifiedTreemap = {
 			};
 
 		if (dir === "h") {
-			nodeBounds.width = areaCalc(node) / rowBounds.height;
-			setBounds(node, nodeBounds);
+			nodeBounds.width = nodeOps.getDisplayArea(node) / rowBounds.height;
+			nodeOps.setBounds(node, nodeBounds);
 			rowBounds.x += nodeBounds.width;
 			rowBounds.width -= nodeBounds.width;
 		} else {
-			nodeBounds.height = areaCalc(node) / rowBounds.width;
-			setBounds(node, nodeBounds);
+			nodeBounds.height = nodeOps.getDisplayArea(node) / rowBounds.width;
+			nodeOps.setBounds(node, nodeBounds);
 			rowBounds.y += nodeBounds.height;
 			rowBounds.height -= nodeBounds.height;
 		}
 
-		this.layoutNodes(row, rowBounds, dir, areaCalc, setBounds);
+		this.layoutNodes(row, rowBounds, dir, nodeOps);
 	},
 	
 	minMaxSum: function minMaxSum(areaCalc) {
-		return function(previousValue, currentValue, index, array) {
+		return function (previousValue, currentValue, index, array) {
 			return { 
-				min:Math.min(previousValue.min,areaCalc(currentValue)), 
-				max:Math.max(previousValue.max,areaCalc(currentValue)),
-				sum:previousValue.sum + areaCalc(currentValue)
+				min: Math.min(previousValue.min, areaCalc(currentValue)), 
+				max: Math.max(previousValue.max, areaCalc(currentValue)),
+				sum: previousValue.sum + areaCalc(currentValue)
 			};
 		};
+	},
+	
+	shallowCopy: function shallowCopy(obj) {
+		var copy = (this instanceof Array) ? [] : {},
+			prop;
+		
+		for (prop in obj) {
+			if (obj.hasOwnProperty(prop)) {
+				copy[prop] = obj[prop];
+			}
+		} 
+		
+		return copy;
 	}
+	
 };
