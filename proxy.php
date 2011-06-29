df3bcf77403f2e51829468d04f3085e08ae8edbd
Kristopher Urie <kristopher@urie.us>
2011-06-29 12:23:44 -0500

    add ubio to proxy

--- proxy.php
+++ proxy.php
@@ -138,7 +138,7 @@
 // Change these configuration options if needed, see above descriptions for info.
 $enable_jsonp    = true;
 $enable_native   = true;
-$valid_url_regex = '@^(http://)?(www\.)?(eol|tolweb|treebase)\.org/@i';
+$valid_url_regex = '@^(http://)?(www\.)?(eol|tolweb|treebase|ubio)\.org/@i';
 
 // ############################################################################
 
