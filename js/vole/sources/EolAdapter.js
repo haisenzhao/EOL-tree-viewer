0a858eb9d20ed395672c52a465517198106dc3d3
Kristopher Urie <kurie@fieldmuseum.org>
2011-08-04 11:59:06 -0500

    set the areamodel to use the node's subtree size, if the templateAdapter offers it, otherwise area=1

--- js/vole/sources/EolAdapter.js
+++ js/vole/sources/EolAdapter.js
@@ -83,10 +83,11 @@ function EolAdapter() {
 		//total_descendants does not include this node, but the media stats (total_descendants_with_text, etc) *do* appear to include it
 		return node && node.total_descendants + 1;
 	};
-//	
-//	this.getNumberOfLeaves = function(node) {
-//		//no way to determine this from the eol api response
-//	}
+	
+	this.getNumberOfLeaves = function(node) {
+		//no way to determine this from the eol api response
+		return 1;
+	}
 }
 
 EolAdapter.prototype = new vole.TreeAdapter();
